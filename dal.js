bc7cdda6e8e4085bb9f58624b405f0a1826fc194
Candace Moorer <candypattie@me.com>
2017-09-05 00:25:58 -0400

    Login files

---
+++ dal.js
@@ -0,0 +1,24 @@
+const users = [
+  { id: 1, name: 'Harry Potter', username: 'harpot', password: 'hogwarts'},
+  {id: 2, name: 'Ron Weasley', username: 'weeze', password: 'gingerhouse'}
+]
+function getUser(userId) {
+  const foundUser = users.find(usr => Number(userId) === usr.id)
+  return foundUser
+
+}
+
+function getUserByUsername (usrname) {
+  const foundUser = users.find(usr => usrname === usr.username)
+  return foundUser
+}
+function getUserByUsername (usrname) {
+  const foundUser = users.find(usr => usrname === usr.username)
+  return foundUser
+}
+
+function getUsers () {
+  return users
+}
+
+module.exports ={ getUser, getUserByUsername, getUsers }
